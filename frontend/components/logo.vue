7c83d8a5fff25bf950ca5ca25a00fa6314259877
luanAfons0 <luan.hmatao@gmail.com>
2025-07-01 21:01:39 -0300

    update(frontend): add basic login page

---
+++ frontend/components/logo.vue
@@ -0,0 +1,8 @@
+<template>
+  <a
+    href="/"
+    class="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white p-3 bg-white flex align-center justify-center rounded-full"
+  >
+    <img class="w-8 h-8" src="/public/logo.svg" alt="logo" />
+  </a>
+</template>
