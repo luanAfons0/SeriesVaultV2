1207b470db2114a788ae2198741fa0e50b455d5c
luanAfons0 <luan.hmatao@gmail.com>
2025-07-05 17:34:33 -0300

    update(backend): remove unnecessary code

---
+++ backend/SeriesVault/Controllers/AccountController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SeriesVault.Controllers
+{
+    [ApiController]
+    [Route("api/")]
+    public class AccountController() : ControllerBase
+    {
+        [HttpGet("account")]
+        public ActionResult GetSeries()
+        {
+            return Ok();
+        }
+    }
+}
--- backend/SeriesVault/Program.cs
+++ backend/SeriesVault/Program.cs
@@ -1,7 +1,4 @@
-using Microsoft.EntityFrameworkCore;
 using Scalar.AspNetCore;
-using SeriesVault.Data;
-using SeriesVault.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,13 +7,6 @@ builder.Services.AddControllers();
 
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
-builder.Services.AddScoped<SeriesService, SeriesService>();
-
-// Configure the Data Base connection and implements the DB service
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-if (connectionString != null)
-    builder.Services.AddDbContext<SeriesDbContext>
-        (options => options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
 
 var app = builder.Build();
 
