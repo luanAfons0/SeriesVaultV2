7dec30beb0f5d88fd0252e9e6adf396a08f352ac
luanAfons0 <luan.hmatao@gmail.com>
2025-07-05 15:14:52 -0300

    update(frontend): add home link to the header

--- frontend/src/components/header/header.tsx
+++ frontend/src/components/header/header.tsx
@@ -5,12 +5,14 @@ import { Link } from "@builder.io/qwik-city";
 export const Header = component$(() => {
   return (
     <header class={styles.container}>
-      <h2>SeriesVault</h2>
+      <Link href="/" aria-label="Go to the home page">
+        <h2>SeriesVault</h2>
+      </Link>
       <div class={styles.buttonRow}>
-        <Link href="/login">
+        <Link href="/login" aria-label="Go to te login page">
           <button class="secondary">Login</button>
         </Link>
-        <Link href="/sign-up">
+        <Link href="/sign-up" aria-label="Go to the sign up page">
           <button>Sign up</button>
         </Link>
       </div>
