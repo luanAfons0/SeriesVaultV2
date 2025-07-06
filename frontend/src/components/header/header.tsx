1bd9f4b38d43ba9051b0bf9b80b4fec3762f3c7e
luanAfons0 <luan.hmatao@gmail.com>
2025-07-06 20:26:25 -0300

    update(frontend): add dropdown menu to the header

--- frontend/src/components/header/header.tsx
+++ frontend/src/components/header/header.tsx
@@ -1,7 +1,7 @@
 import { $, component$, useOnDocument, useSignal } from "@builder.io/qwik";
 import styles from "./styles.module.scss";
 import { Link } from "@builder.io/qwik-city";
-import { TbSun, TbMoon } from "@qwikest/icons/tablericons";
+import { TbSun, TbMoon, TbMenu2 } from "@qwikest/icons/tablericons";
 
 export const Header = component$(() => {
   const $htmlElement = useSignal<HTMLElement | null>(null);
@@ -36,21 +36,32 @@ export const Header = component$(() => {
       <Link href="/" aria-label="Go to the home page">
         <h2>SeriesVault</h2>
       </Link>
-      <div class={styles.buttonRow}>
-        <Link href="/login/" aria-label="Go to te login page">
-          <button class="secondary">Login</button>
-        </Link>
-        <Link href="/sign-up/" aria-label="Go to the sign up page">
-          <button>Sign up</button>
-        </Link>
-        <button
-          class={`${getButtonColor()} ${styles.themeButton}`}
-          onClick$={() => handleSwitchTheme()}
-          aria-label="changeTheme"
-        >
-          {theme.value == "dark" ? <TbSun /> : <TbMoon />}
-        </button>
-      </div>
+      <details class="dropdown">
+        <summary>
+          <TbMenu2 />
+        </summary>
+        <ul dir="rtl">
+          <li>
+            <Link href="/login/" aria-label="Go to te login page">
+              <button class="secondary">Login</button>
+            </Link>
+          </li>
+          <li>
+            <Link href="/sign-up/" aria-label="Go to the sign up page">
+              <button>Sign up</button>
+            </Link>
+          </li>
+          <li>
+            <button
+              class={`${getButtonColor()} ${styles.themeButton}`}
+              onClick$={() => handleSwitchTheme()}
+              aria-label="changeTheme"
+            >
+              {theme.value == "dark" ? <TbSun /> : <TbMoon />}
+            </button>
+          </li>
+        </ul>
+      </details>
     </header>
   );
 });
